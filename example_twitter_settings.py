7c8477787c4ed3afb40adf31f446ea34a8848a81
Tom Martin <tompoges@gmail.com>
2013-12-03 23:25:40 +0000

    Example twitter settings

---
+++ example_twitter_settings.py
@@ -0,0 +1,7 @@
+# Fill in the blanks below and rename this file twitter_settings.py
+
+import oauth2 as oauth
+
+screen_name='' # Twitter username
+token = oauth.Token(key="", secret="") # Twitter users token and secret
+consumer = oauth.Consumer(key="", secret="") # Key and secret of the twitter appliction
